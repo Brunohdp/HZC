b9ef723fdceb384cb25f6ca1616a3cb80b59cfe1
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2022-02-11 13:40:15 -0300

    Organização
    Coloquei o css das notificações em um arquivo separado, adicionei ';' no JS que tinha esquecido e voltei o HTML das notificações para baixo no menu como estava antes

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-const menuLateral = document.querySelector(".menu-lateral")
-const botaoMenu = document.querySelector(".cabecalho__menu")
-const areaNoticacao = document.querySelector(".notificacoes")
-const botaoNotificacao = document.querySelector(".cabecalho__notificacao")
+const menuLateral = document.querySelector(".menu-lateral");
+const botaoMenu = document.querySelector(".cabecalho__menu");
+const areaNoticacao = document.querySelector(".notificacoes");
+const botaoNotificacao = document.querySelector(".cabecalho__notificacao");
 
 botaoMenu.addEventListener("click", () => {
-  menuLateral.classList.toggle('menu-lateral--ativo')
+  menuLateral.classList.toggle('menu-lateral--ativo');
 })
 
 botaoNotificacao.addEventListener("click", () => {
-  areaNoticacao.classList.toggle('notificacoes--ativo')
+  areaNoticacao.classList.toggle('notificacoes--ativo');
 })
