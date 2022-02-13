cf3d1b0f85a5b5a13ec3c6b3cc243bd915574468
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2022-02-13 16:11:24 -0300

    cursor pointer
    Coloquei o cursor como pointer para que sempre que o mouse estiver em cima dos botões ele apareça como a mãozinha. Também comecei o código no JS que redireciona para o link da página referente ao botão clicado

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const menuLateral = document.querySelector(".menu-lateral");
 const botaoMenu = document.querySelector(".cabecalho__menu");
 const areaNoticacao = document.querySelector(".notificacoes");
 const botaoNotificacao = document.querySelector(".cabecalho__notificacao");
+const botaoVideoDestaque = document.querySelector(".cartao__botao--destaque")
+
 
 botaoMenu.addEventListener("click", () => {
   menuLateral.classList.toggle('menu-lateral--ativo');
@@ -9,4 +11,8 @@ botaoMenu.addEventListener("click", () => {
 
 botaoNotificacao.addEventListener("click", () => {
   areaNoticacao.classList.toggle('notificacoes--ativo');
+})
+
+botaoVideoDestaque.addEventListener("click", () => {
+  window.location.href="https://www.google.com"
 })
